326f6dd8845b0b396459ef24845f3318343d105f
Lorenz De Bie <lorenz.debie@student.hogent.be>
2022-03-05 14:17:19 +0100

    my-sql connector toegevoegd, + code eein klein beetje aangepast voor de connectie

--- src/persistence/Connectie.java
+++ src/persistence/Connectie.java
@@ -1,5 +1,29 @@
 package persistence;
 
 public class Connectie {
-    public static final String CONNECTION_URL = "jdbc//mysql//ID372560_SDProjectG101.db.webhosting.be&user=ID372560_SDProjectG101&password=SDPdatabankG101";
+    public static final String JDBC_URL = "jdbc:mysql://ID372560_SDProjectG101.db.webhosting.be&user=ID372560_SDProjectG101&password=SDPdatabankG101";
 }
+/*
+Connection con = null;
+
+String url = "jdbc:mysql://localhost:3306/some_db";
+String username = "some_username":
+String password = "some_password";
+
+try {
+  Class.forName("com.mysql.jdbc.Driver");
+  con = DriverManager.getConnection(url, username, password);
+
+  System.out.println("Connected!");
+
+} catch (SQLException ex) {
+    throw new Error("Error ", ex);
+} finally {
+  try {
+    if (con != null) {
+        con.close();
+    }
+  } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+  }
+}*/
