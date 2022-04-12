fae71c51ed45400d04b581f62d24f48a7d1d01e4
Andreeas FIroiu <andrfiro@gmail.com>
2022-04-12 22:01:35 +0200

    VakTest

---
+++ src/testen/VakTest.java
@@ -0,0 +1,24 @@
+package testen;
+
+import domein.Vak;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class VakTest {
+    @Test
+    @DisplayName("Toegelaten vakje, happy flow")
+    void create_happyflow(){
+        assertDoesNotThrow(() -> new Vak(8,8) );
+    }
+
+
+    @Test
+    @DisplayName("Niet toegelaten vakje, throws exception")
+    void create_VakBuitenBord_Exception(){
+        assertThrows(IllegalArgumentException.class,() -> new Vak(16,16) );
+    }
+}
