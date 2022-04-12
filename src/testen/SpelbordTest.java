90fdd4f70239e202b598dccbd7d9c9e4281939f0
Andreeas FIroiu <andrfiro@gmail.com>
2022-04-12 21:57:22 +0200

    afwerken spelbordtest

--- src/testen/SpelbordTest.java
+++ src/testen/SpelbordTest.java
@@ -5,14 +5,14 @@ import domein.Vak;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SpelbordTest {
 
     @Test
     @DisplayName("Maak spelbord, correcte vakjes gegenereerd streekproef")
-    public void create_vakjesCorrect() {
+    public void create_VakjesCorrect() {
         Spelbord testBord = new Spelbord();
         Vak testVak1 = testBord.getVakjes().get("7.4");
         Vak testVak2 = testBord.getVakjes().get("9.15");
@@ -23,6 +23,12 @@ public class SpelbordTest {
         assertEquals(15,testVak2.getRij());
         assertEquals(Vak.Kleur.ZWART,testVak1.getKleur());
         assertEquals(Vak.Kleur.WIT,testVak2.getKleur());
+    }
 
+    @Test
+    @DisplayName("Maak spelbord, vraag onbestaand vakje, is null")
+    public void create_VraagIncorrectVakje_null() {
+        Spelbord testBord = new Spelbord();
+        assertNull(testBord.getVakjes().get("16.16"));
     }
 }
