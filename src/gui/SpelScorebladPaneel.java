fce0f0cce4eff79328cd8e490d7d14f20b1ed5b5
Andreeas Firoiu <andrfiro@gmail.com>
2022-05-05 20:31:15 +0200

    kleine fix

--- src/gui/SpelScorebladPaneel.java
+++ src/gui/SpelScorebladPaneel.java
@@ -4,16 +4,16 @@ import domein.DomeinController;
 import javafx.beans.value.ChangeListener;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 
 public class SpelScorebladPaneel extends VBox {
 
     private final DomeinController domeinController;
     private Label lblTitel;
     private Label lblActieveSpeler;
-    private TextField txfScoreblad;
+    private Text txfScoreblad;
 
     /**
      * UC3: constructor voor paneel
@@ -35,24 +35,12 @@ public class SpelScorebladPaneel extends VBox {
         // instantie elementen
         lblTitel = new Label(domeinController.getTaal().getLocalisatie("HUIDIGE_SPELER"));
         lblActieveSpeler = new Label();
-        txfScoreblad = new TextField();
-           
-        
-        //mogelijks werkt dit om de textfield automatisch te laten groeien
-     //  txfScoreblad.prefColumnCountProperty().bind(txfScoreblad.textProperty().length());
-       //setVgrow(txfScoreblad, Priority.ALWAYS);
+        txfScoreblad = new Text();
 
-        // eventlisteners
-        txfScoreblad.textProperty().addListener((ChangeListener<String>) (observable, oldValue,
-                                                                          newValue) -> txfScoreblad.setPrefWidth(txfScoreblad.getText().length() * 7));
 
         // styling
         lblTitel.setStyle("-fx-font-size: 2em");
         lblActieveSpeler.setStyle("-fx-font-size: 1.5em");
-       txfScoreblad.setAlignment(Pos.CENTER);
-        txfScoreblad.setMaxWidth(300);
-      //  txfScoreblad.setMinHeight(500);
-        this.txfScoreblad.setEditable(false);
         this.setPrefHeight(1000);
         this.setPrefWidth(350);
         this.setAlignment(Pos.BASELINE_LEFT);
